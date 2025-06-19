0ffa805994ec60a8f80fb40eeb3aadc4cc704c38
sahana-athota <sahana.athota@gmail.com>
2025-06-19 18:48:48 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: '/sahana-athota.github.io/',  // replace this
+  plugins: [react()],
 });
